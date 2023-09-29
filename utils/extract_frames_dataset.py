fbc4952dcfaaadaba24a1ca44d343c80e400c7fb
Bernardo Biesseck <biesseck@gmail.com>
2023-09-29 14:37:00 -0300

    utils/extract_frames_dataset.py: add paths of dataset OULU-NPU

--- utils/extract_frames_dataset.py
+++ utils/extract_frames_dataset.py
@@ -3,13 +3,22 @@ import utils
 
 if __name__ == '__main__':
 
-    # dataset OULU-NPU
-    input_folder_path = ['/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/oulu-npu/train',
-                         '/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/oulu-npu/test',
-                         '/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/oulu-npu/dev']
-    desired_extensions = ['.avi']
+    # # dataset OULU-NPU
+    # input_folder_path = ['/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/oulu-npu/train',
+    #                      '/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/oulu-npu/test',
+    #                      '/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/oulu-npu/dev']
+    # desired_extensions = ['.avi']
+    # output_folder_path = '/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/oulu-npu_frames'
+    
 
-    output_folder_path = '/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/oulu-npu_frames'
+    # dataset replay-attack
+    input_folder_path = ['/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/replay-attack/train',
+                         '/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/replay-attack/devel',
+                         '/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/replay-attack/test']
+    desired_extensions = ['.mov']
+    output_folder_path = '/experiments/BOVIFOCR_project/datasets/bjgbiesseck/liveness/replay-attack_frames'
+    
+
     frame_idx_to_extract=[0]
 
     for input_folder in input_folder_path:
